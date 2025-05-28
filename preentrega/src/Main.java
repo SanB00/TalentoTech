02e24d2f1f5398dff7d49189535b9c5f80b57862
Santi B <151892199+SanB00@users.noreply.github.com>
2025-05-28 19:29:21 -0300

    implementación de "listarPedidos".

--- preentrega/src/Main.java
+++ preentrega/src/Main.java
@@ -1,15 +1,17 @@
 
 
+import excepciones.StockInsuficienteException;
+import utils.Utils;
+
 import java.util.ArrayList;
 import java.util.Scanner;
 
 
-
 public class Main {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
         Scanner entrada = new Scanner(System.in);
-        ArrayList<Producto> productos = new ArrayList<>();
+        ArrayList<Producto> listaProductos = new ArrayList<>();
         ArrayList<Pedido> listaPedidos = new ArrayList<>();
         int opcion = 0;
 
@@ -19,11 +21,14 @@ public class Main {
             opcion = entrada.nextInt();
             entrada.nextLine();
             switch (opcion) {
-                case 1 -> agregarProducto(productos);
-                case 2 -> listarProductos(productos);
-                case 3 -> buscarProducto(productos);
-                case 4 -> eliminarProducto(productos);
-                case 5 -> agregarPedido(listaPedidos);
+                case 1 -> agregarProducto(listaProductos);
+                case 2 -> listarProductos(listaProductos);
+                case 3 -> buscarProducto(listaProductos);
+                case 4 -> eliminarProducto(listaProductos);
+                case 5 -> {
+                    Pedido objPedido = crearPedido(listaProductos);
+                    listaPedidos.add(objPedido);
+                }
                 case 6 -> listarPedidos(listaPedidos);
                 case 7 -> System.out.println("Gracias por usar la app!");
                 default -> System.out.println("Opcion incorrecta");
@@ -34,6 +39,7 @@ public class Main {
     }
 
     public static void mostrarMenu() {
+        Utils.clearScreen();
         System.out.println("""
                 Menu principal:
                 
@@ -47,7 +53,7 @@ public class Main {
                 """);
     }
 
-    public static void agregarProducto(ArrayList<Producto> productos) {
+    public static void agregarProducto(ArrayList<Producto> listaProductos) throws StockInsuficienteException {
         Scanner entrada = new Scanner(System.in);
         System.out.println("Menu para agregar un producto.");
         System.out.println("Ingrese el nombre del producto: ");
@@ -58,29 +64,29 @@ public class Main {
         int stock = entrada.nextInt();
         Producto producto = new Producto(nombre, precio, stock);
 
-        productos.add(producto);
+        listaProductos.add(producto);
 
         System.out.println("Producto cargado exitosamente! ");
     } //1
 
-    private static void listarProductos(ArrayList<Producto> productos) {
-        if (productos.isEmpty()) {
+    private static void listarProductos(ArrayList<Producto> listaProductos) {
+        if (listaProductos.isEmpty()) {
             System.out.println("No hay producto todavia :(");
         } else {
-            for (Producto producto : productos) {
+            for (Producto producto : listaProductos) {
                 producto.mostrarInfo();
             }
         }
     } //2
 
-    private static void buscarProducto(ArrayList<Producto> productos) {
+    private static void buscarProducto(ArrayList<Producto> listaProductos) {
         System.out.println("Buscador de productos.");
         System.out.println("Ingrese nombre del producto:");
         Scanner entrada = new Scanner(System.in);
         String busqueda = entrada.nextLine();
         ArrayList<Producto> productosEncontrados = new ArrayList<>();
 
-        for (Producto producto : productos) {
+        for (Producto producto : listaProductos) {
             if (producto.contieneNombre(busqueda)) {
                 productosEncontrados.add(producto);
             }
@@ -95,13 +101,13 @@ public class Main {
         }
     } //3
 
-    private static void eliminarProducto(ArrayList<Producto> productos) {
+    private static void eliminarProducto(ArrayList<Producto> listaProductos) {
         System.out.println("\n=== Eliminar Producto ===");
         System.out.print("Ingrese ID del producto a eliminar: ");
         Scanner entrada = new Scanner(System.in);
         int id = Integer.parseInt(entrada.nextLine().trim());
         Producto productoAEliminar = null;
-        for (Producto producto : productos) {
+        for (Producto producto : listaProductos) {
             if (producto.getId() == id) {
                 productoAEliminar = producto;
                 break;
@@ -109,33 +115,58 @@ public class Main {
         }
 
         if (productoAEliminar != null) {
-            productos.remove(productoAEliminar);
+            listaProductos.remove(productoAEliminar);
             System.out.println("Producto eliminado exitosamente.");
         } else {
-            System.out.println("No se encontró un producto con ese ID.");
+            System.out.println("No se encontró un producto con ese ID= " + id);
         }
     } // 4
 
-    private static void agregarPedido(ArrayList<Pedido> pedidos) {
+    public static Producto buscarProductoId(ArrayList<Producto> listaProductos, int id) {
+        for (Producto producto : listaProductos) {
+            if (producto.getId() == id) {
+                return producto;
+            }
+        }
+        return null;
+    }
+
+    private static Pedido crearPedido(ArrayList<Producto> listaProductos) throws Exception {
+        Pedido objPedido = new Pedido();
+        // 1) Primer paso obtener los datos de ingreso
         Scanner entrada = new Scanner(System.in);
         System.out.println("Generando un nuevo pedido\n=======");
-        while(true){
-        System.out.println("Ingrese id del producto: ");
-        String strIdProducto = entrada.nextLine();
-        System.out.println("Ingrese cantidad que desea: ");
-        String strCantidad = entrada.nextLine();
-        int cantidad=0;
-        cantidad=Integer.parseInt(strCantidad);
+        while (true) {
+            System.out.println("Ingrese id del producto: ");
+            String strIdProducto = entrada.nextLine();
+            int idProducto = 0;
+            idProducto = Integer.parseInt(strIdProducto);
+            if (idProducto == 0) {
+                break;
+            }
+            // 2) Validar que esté en formato correspondiente
 
-        int idProducto=0;
-        idProducto=Integer.parseInt(strIdProducto);
-        if(idProducto==0){
-           break;
+            // 3) Crear un objeto producto para que guarde los ingresos
+
+            Producto objProducto = buscarProductoId(listaProductos, idProducto);
+            if (objProducto == null)
+                throw new Exception("Prod inexsitente");
+
+
+            System.out.println("Ingrese cantidad que desea: ");
+            String strStock = entrada.nextLine();
+            int stock = 0;
+            stock = Integer.parseInt(strStock);
+
+
+            // 4) Agregar el objeto de pedido al listado
+            objPedido.agregarProductoAPedido(objProducto);
         }
-        }
 
-
+        // 5) Devolver el pedido
+        return objPedido;
     }
+
     private static void listarPedidos(ArrayList<Pedido> listaPedidos) {
         if (listaPedidos.isEmpty()) {
             System.out.println("No hay pedidos registrados.");
--- preentrega/src/Pedido.java
+++ preentrega/src/Pedido.java
@@ -1,66 +1,32 @@
 
 import java.util.ArrayList;
-import java.util.List;
 
 public class Pedido {
 
-    private List<Producto> productos;
+    private ArrayList<Producto> listaProductos;
     private static int SIGUIENTE_ID = 1;
     private final int id;
-    private String nombre;
-    private double precio;
-    private int stock;
-    private int cantidadAComprar;
 
     public Pedido() {
-        this.productos = new ArrayList<>();
-        this.nombre = nombre;
-        this.precio = precio;
-        this.stock = stock;
-        this.cantidadAComprar = 0;
+        this.listaProductos = new ArrayList<>();
         this.id = SIGUIENTE_ID;
         SIGUIENTE_ID++;
     }
 
-    public void agregarProductoAPedido(Producto producto){
-        this.productos.add(producto);
+    public void agregarProductoAPedido(Producto producto) {
+
+        this.listaProductos.add(producto);
     }
+
     public void mostrarInfo() {
         System.out.println("#########################");
         System.out.printf("""
                 Id: %s
-                Nombre: %s
-                Precio: %s
-                Stock: %s
-                """, this.id, this.nombre, this.precio, this.stock);
+                """, this.id);
         System.out.println("#########################");
     }
 
     public int getId() {
         return id;
-    }
-
-    public String getNombre() {
-        return nombre;
-    }
-
-    public void setNombre(String nombre) {
-        this.nombre = nombre;
-    }
-
-    public double getPrecio() {
-        return precio;
-    }
-
-    public void setPrecio(double precio) {
-        this.precio = precio;
-    }
-
-    public int getStock() {
-        return stock;
-    }
-
-    public void setStock(int stock) {
-        this.stock = stock;
     }
 }
